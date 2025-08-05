a0ca3a514122dea4563e24aea0625f1f02996832
Julian Anthony Braun <jbraun3@uw.edu>
2025-08-04 20:30:43 -0700

    semantic react now displays

--- hws-website/src/app/App.jsx
+++ hws-website/src/app/App.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
-import './Global.css'
-
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { } from "react-router-dom";
+
+import '../assets/Global.css'
+
+import Home from './routes/Home.jsx';
 
 // App structure and components
 function App() {
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="lessons">
+          {/* <Route path="lessons">
             <Route index element={<LessonsInfo />} />
             <Route path="lesson-registration" element={<LessonRegistration />} />
             <Route path="join-our-mailing-list" element={<JoinMailingList />} />
@@ -25,10 +27,10 @@ function App() {
           <Route path="questions">
             <Route index element={<FAQ />} />
             <Route path="contact-us" element={<ContactUs />} /> 
-          </Route>
+          </Route> */}
         </Routes>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
